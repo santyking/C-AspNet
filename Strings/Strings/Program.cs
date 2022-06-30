3ce3dbdc0e3dca3d9eab6f795936d89f4ba02ad0
santyking <santosh.mhatre93@gmail.com>
2022-06-30 18:37:53 +0530

    C# variables and Strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String Phrase = "Santosh Corps";
+            Console.WriteLine("Hello \n World!");
+            Console.WriteLine("Hello \" World!");
+            Console.WriteLine(Phrase+" is cool");
+            Console.WriteLine("Length is "+Phrase.Length);
+            Console.WriteLine(Phrase.ToUpper());
+            Console.WriteLine(Phrase.Contains("Santosh"));
+            Console.WriteLine(Phrase[2]);
+            Console.WriteLine(Phrase.IndexOf('t'));
+            Console.WriteLine(Phrase.IndexOf("Corpso"));
+            Console.WriteLine(Phrase.Substring(9));
+            Console.WriteLine(Phrase.Substring(Phrase.IndexOf("Corps")));
+        }
+    }
+}
---
+++ VariablesDataTypes/VariablesDataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace VariablesDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 28;
+            String name = "Santosh";
+            Console.WriteLine("Hi! i am "+name+" . I am "+age+" years old.");
+        }
+    }
+}
