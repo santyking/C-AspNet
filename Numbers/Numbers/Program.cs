28593dae2289590b0344d2a76216ec8f591ffba9
santyking <santosh.mhatre93@gmail.com>
2022-07-03 21:09:40 +0530

    Numbers in C#

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine(9);
+            Console.WriteLine(2 + 2);
+            Console.WriteLine(Math.Abs(-7));
+            Console.WriteLine(Math.Pow(3,2));
+            Console.WriteLine(Math.Sqrt(36));
+            int num1 = 4;
+            int num2 = 5;
+            Console.WriteLine(Math.Max(num1, num2));
+            */
+            Console.WriteLine(Math.Round(2.6));
+        }
+    }
+}
