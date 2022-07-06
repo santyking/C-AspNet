a6b4cd8cab2fdf130fb127c6234fc6cf5e335d45
santyking <santosh.mhatre93@gmail.com>
2022-07-06 12:32:14 +0530

    Getting user inputs as String

---
+++ GettingUserInput/GettingUserInput/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GettingUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Name : ");
+            String name = Console.ReadLine();
+            Console.Write("Enter your age : ");
+            String age = Console.ReadLine();
+            Console.WriteLine("Hi! "+name+". You are "+age+" years old");
+        }
+    }
+}
