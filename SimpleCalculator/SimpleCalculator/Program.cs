5cad84f0f667e5a7f165cbbc676b9cc9b61eb3e1
santyking <santosh.mhatre93@gmail.com>
2022-07-06 12:50:08 +0530

    Simple Calculator

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first Number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second Number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double sum = num1 + num2;
+            double sub = num1 - num2;
+            double mul = num1 * num2;
+            double div = num1 / num2;
+            Console.WriteLine("The Sum is : " + sum);
+            Console.WriteLine("The Diffrence is : " + sub);
+            Console.WriteLine("The Diffrence is : " + mul);
+            Console.WriteLine("The Diffrence is : " + div);
+        }
+    }
+}
