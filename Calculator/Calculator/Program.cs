d5fe76b1bc36d6fad287181de0ac789687008931
santyking <santosh.mhatre93@gmail.com>
2022-07-07 21:21:17 +0530

    Calculator using Switch case

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number : ");
+            decimal num1 = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter Second Number : ");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter Operator : ");
+            String symbol = Console.ReadLine();
+            switch (symbol)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+                case "%":
+                    Console.WriteLine(num1 % num2);
+                    break;
+                default:
+                    Console.WriteLine(symbol + " is not a valid Opearator");
+                    break;
+            }
+        }
+    }
+}
