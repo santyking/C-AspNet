ac1b2ec960396b0a2e38a10126757e0a8aabf390
santyking <santosh.mhatre93@gmail.com>
2022-07-10 03:05:29 +0530

    Better Calculator

---
+++ BetterCalculator/BetterCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BetterCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number : ");
+            decimal num1 = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter Operator : ");
+            String Op = Console.ReadLine();
+            Console.Write("Enter Second Number : ");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+            decimal result = calculator(num1, Op, num2);
+            Console.WriteLine(result);
+        }
+        static decimal calculator(decimal num1 ,String op, decimal num2)
+        {
+            decimal result;
+            switch (op)
+            {
+                case "+":
+                    result = num1+num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "%":
+                    result = num1 % num2;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            return result;
+        }
+    }
+}
