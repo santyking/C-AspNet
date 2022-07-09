c0ba4954d6da630a586ac821d828a95fbb3b970a
santyking <santosh.mhatre93@gmail.com>
2022-07-10 01:22:23 +0530

    Methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Santosh",28);
+            int sumNo = Sum(2, 2);
+            Console.WriteLine(sumNo);
+            int sum3 = Sum(3, 3, 3);
+            Console.WriteLine(sum3);
+
+        }
+
+        static void SayHi(String name,int age)
+        {
+            Console.WriteLine("Hello! "+name+". You are "+age);
+        }
+        static int Sum(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        static int Sum(int num1,int num2,int num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+}
