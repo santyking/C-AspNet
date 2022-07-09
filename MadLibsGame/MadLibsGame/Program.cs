f98a665764eca9c3b1543f4f8df0a9d9dbc309f9
santyking <santosh.mhatre93@gmail.com>
2022-07-09 20:37:20 +0530

    Mad Libs Game

---
+++ MadLibsGame/MadLibsGame/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MadLibsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Noun : ");
+            String noun  = Console.ReadLine();
+            Console.Write("Enter Adjective : ");
+            String adjective  = Console.ReadLine();
+            Console.Write("Enter name : ");
+            String name = Console.ReadLine();
+            Console.WriteLine( noun +" are red");
+            Console.WriteLine("Voilets are "+adjective);
+            Console.WriteLine(name+" love you");
+        }
+    }
+}
